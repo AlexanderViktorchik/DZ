c7a3640649bf784597db4ea4b92f9681773c4d8a
Alexander <s.viktor4ik@mail.ru>
2019-04-23 13:38:31 +0300

    initial commit

---
+++ DZ_1/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let money = prompt("Ваш бюджет на месяц?");
+let timeData = prompt("Введите дату в формате YYYY-MM-DD");
+let answer1 = prompt('Введите обязательную статью расходов в этом месяце');
+let answer2 = prompt('Во сколько обойдется?');
+ 
+
+let appData = {
+    money,
+    timeData,
+    expenses: {answer1 : answer2},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+}
+
+alert('Ваш дневной бюджет: ' + appData.money/30)
