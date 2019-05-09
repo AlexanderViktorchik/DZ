1558babfd770bb606f25beb7247be32f1e529d8c
Alex <s.viktor4ik@mail.ru>
2019-05-09 20:37:50 +0300

    new initial

---
+++ DZ_5/js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+let arrMenu = document.getElementsByTagName('li'),
+    ul = document.getElementsByTagName('ul');
+
+// document.body.insertBefore(arrMenu[2], arrMenu[1]);
+// alert('kdksddo');
+
+console.log(arrMenu);
+console.log(ul);
+arrMenu[1].style.backgroundColor = 'red';
+
+// console.log(arrMenu[1]);
